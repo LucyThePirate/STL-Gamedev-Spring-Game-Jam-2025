bab31051b2ae4e7e846e0be67e5b8da307559305
Walpang <58585553+Walpang@users.noreply.github.com>
2025-05-24 15:23:47 -0700

    Player 1 and 2 Basic Movement
    WASD, IJKL

--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
+@export var player_id = 1
+
 const SPEED = 300.0
 const ACCEL = 2.0
 
 var input: Vector2
 
 func get_input():
-	input.x = Input.get_action_strength("Move P1 Right") - Input.get_action_strength("Move P1 Left")
-	input.y = Input.get_action_strength("Move P1 Down") - Input.get_action_strength("Move P1 Up")
+	input.x = Input.get_action_strength("Move P%s Right" % [player_id]) - Input.get_action_strength("Move P%s Left" % [player_id])
+	input.y = Input.get_action_strength("Move P%s Down" % [player_id]) - Input.get_action_strength("Move P%s Up" % [player_id])
 	return input.normalized()
 
 func _process(delta: float) -> void:
